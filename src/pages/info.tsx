00dc94665a297fc7af55f66572e81f7be6cd9493
wonjung <wonjung1919@gmail.com>
2025-11-22 08:24:01 +0900

    feat: ino

---
+++ src/pages/info.tsx
@@ -0,0 +1,13 @@
+// pages/login.tsx
+import { useRouter } from "next/router";
+
+export default function info() {
+  const router = useRouter();
+
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center bg-[#FFF6EB]">
+
+    <h1>수고하셨습니다.</h1>
+    </div>
+  );
+}
